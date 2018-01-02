16c5cdb2c0855db6f94abfe3724db1fc17ef23cc
Orkun Düzgün <orkundzgn@gmail.com>
2018-01-02 14:36:16 +0300

    Initial Commit

---
+++ app/src/main/java/com/orkunduzgun/flashchatnewfirebase/MainChatActivity.java
@@ -0,0 +1,58 @@
+package com.orkunduzgun.flashchatnewfirebase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+
+public class MainChatActivity extends AppCompatActivity {
+
+    // TODO: Add member variables here:
+    private String mDisplayName;
+    private ListView mChatListView;
+    private EditText mInputText;
+    private ImageButton mSendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat);
+
+        // TODO: Set up the display name and get the Firebase reference
+
+
+        // Link the Views in the layout to the Java code
+        mInputText = (EditText) findViewById(R.id.messageInput);
+        mSendButton = (ImageButton) findViewById(R.id.sendButton);
+        mChatListView = (ListView) findViewById(R.id.chat_list_view);
+
+        // TODO: Send the message when the "enter" button is pressed
+
+
+        // TODO: Add an OnClickListener to the sendButton to send a message
+
+    }
+
+    // TODO: Retrieve the display name from the Shared Preferences
+
+
+    private void sendMessage() {
+
+        // TODO: Grab the text the user typed in and push the message to Firebase
+
+    }
+
+    // TODO: Override the onStart() lifecycle method. Setup the adapter here.
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // TODO: Remove the Firebase event listener on the adapter.
+
+    }
+
+}
