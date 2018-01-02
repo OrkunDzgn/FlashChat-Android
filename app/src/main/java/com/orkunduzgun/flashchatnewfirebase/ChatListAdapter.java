588fb2812f3d4d82cc71ea8aded7a53f59117ebf
Orkun Düzgün <orkundzgn@gmail.com>
2018-01-02 22:07:44 +0300

    styled messages

--- app/src/main/java/com/orkunduzgun/flashchatnewfirebase/ChatListAdapter.java
+++ app/src/main/java/com/orkunduzgun/flashchatnewfirebase/ChatListAdapter.java
@@ -2,7 +2,9 @@ package com.orkunduzgun.flashchatnewfirebase;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -107,6 +109,9 @@ public class ChatListAdapter extends BaseAdapter {
         final InstantMessage message = getItem(position);
         final ViewHolder holder = (ViewHolder) convertView.getTag();
 
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe, holder);
+
         String author = message.getAuthor();
         holder.authorName.setText(author);
 
@@ -114,6 +119,21 @@ public class ChatListAdapter extends BaseAdapter {
         holder.body.setText(body);
 
         return convertView;
+    }
+
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder) {
+        if (isItMe) {
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        }
+        else {
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
     }
 
     public void cleanup() {
